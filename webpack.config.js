92c767d2906d6e767e93f39c0197b10aceae27bf
Huzefa Jambughoda <31165049+huzefa26@users.noreply.github.com>
2020-10-22 01:46:18 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/Stock-Trader---VueJS-App/dist/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
